281058a24fcbf34a77a783519b21818d75e11b76
COMLAB <lcacapol23-0372@cca.edu.ph>
2025-02-12 17:51:16 +0800

    added to new php
    JUST FOR EXAMPLE

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head></head>
+    <body>
+        <h1>
+        <?php echo "HELLO WORLD!"; ?> <br>
+        <?php
+            $variable1 = 10;
+            $variable2 = 5;
+        ?>
+
+        Num1: <?php echo $variable1; ?> <br>
+        Num2: <?php echo $variable2; ?> <br>
+        Sum of Num1 and Num2: <?php echo $variable1 + $variable2; ?> <br>
+        </h1>
+    </body>
+</html>
